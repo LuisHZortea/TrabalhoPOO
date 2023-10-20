7198c21be7f07026e04156ffa306d624d9a38fcb
Filipe Braga <102600194+Filipe-Braga@users.noreply.github.com>
2023-10-20 20:24:11 -0300

    Update Script.js
    Mais um BO resolvido 😎👍

--- Script.js
+++ Script.js
@@ -1,23 +1,14 @@
-// This is your Prisma schema file,
-// learn more about it in the docs: https://pris.ly/d/prisma-schema
-
-generator client {
-  provider = "prisma-client-js"
-}
-
-datasource db {
-  provider = "sqlite"
-  url      = env("DATABASE_URL")
-}
-
 model Aluno {
   id Int @id @default(autoincrement())
   email String
+  //senha String
   curso String
+  lider Boolean
   matriculaLider Grupo? @relation(fields: [matricula], references: [lider])
   matricula String @unique
   matriculaAvaliador Avaliador?
 }
+
 
 model Grupo {
   nome String
@@ -25,11 +16,9 @@ model Grupo {
   numeroEstande Int
   dataHora DateTime
   alunos Aluno[]
+  avaliacao Avaliacao []
   lider String @unique
   estande Estande @relation(fields: [numeroEstande], references: [numeroEstande])
-  //ISSO AQUI EMBAIXO ESTA COM ERRO
-  avaliacao Avaliacao @relation(fields: [numeroGrupo], references: [nota])
-
 }
 
 model Professor {
@@ -37,16 +26,15 @@ model Professor {
   matricula String @unique
   nome String
   matriculaAvaliador Avaliador?
-  // matriculaAvaliacao Avaliacao? @relation(fields: [matricula], references: [matriculaAvaliador])
 }
 
 model Avaliacao {
   id Int @id @default(autoincrement())
+  grupo Grupo @relation(fields: [id], references: [numeroGrupo])
   nota Int
-  grupo Grupo
 }
 
-model Avaliador {
+model Avaliador{
   tipoAvaliador String
   matricula String @unique
   aluno Aluno? @relation(fields: [matricula], references: [matricula])
@@ -56,4 +44,8 @@ model Avaliador {
 model Estande {
   numeroEstande Int @id
   grupos Grupo[] 
+ 
 }
+
+
+
